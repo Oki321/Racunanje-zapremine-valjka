229c8273916ebf07ed89f7ec7b2ed5317c39ffce
Oki321 <118111175+Oki321@users.noreply.github.com>
2023-03-20 20:24:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let poluprecnik = prompt("Unesite poluprečnik valjka:")
+const pi = 3.14
+let visina = prompt("Unesite visinu:")
+
+let v = (parseInt(poluprecnik) * parseInt(poluprecnik)) * pi * visina
+alert("Zapremina valjka je: " + v)
